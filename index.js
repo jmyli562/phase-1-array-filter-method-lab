5938131a8082a9dff13d4052280f10d280196260
Jimmy Li <jmyli562@gmail.com>
2023-03-25 15:03:57 -0700

    finishes lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution here
+function findMatching(drivers, name){
+    let newArray = [];
+    newArray = drivers.filter(function(element){
+        if(name === element || name.toUpperCase() === element.toUpperCase()){
+            return true;
+        }else{
+            return false;
+        }
+    })
+
+    return newArray;
+
+}
+
+function fuzzyMatch(drivers, letters){
+    let stringLength = letters.length;
+    let newArray = drivers.filter(function(element){
+        if(element.slice(0,stringLength) === letters){
+            return true;
+        }else{
+            return false;
+        }})
+
+    return newArray;
+}
+
+
+function matchName(drivers, string){
+    let newArray = drivers.filter(function(element){
+        if(element.name === string){
+            return true;
+        }else{
+            return false;
+        }
+    })
+
+    return newArray;
+}
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+console.log(fuzzyMatch(drivers, 'Sa'))
